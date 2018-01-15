1cab70abd7337137cd74bac3d86814dc3ef6a04c
k15015kk <aitk15015kk@outlook.com>
2018-01-16 00:38:49 +0900

    変更

---
+++ JsonDownloadParse/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using GeoJSON.Net;
+
+namespace JsonDownloadParse
+{
+    public class LailData {
+        public List<Features> Features {
+            get;
+            set;
+        }
+    }
+    
+    public class Features
+    {
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        public Properties Properties
+        {
+            get;
+            set;
+        }
+
+        public Geometory Geometry {
+            get;
+            set;
+        }
+    }
+
+    public class Properties {
+        public string Classnumber {
+            get;
+            set;
+        }
+
+        public string Operatordist {
+            get;
+            set;
+        }
+
+        public string Linename {
+            get;
+            set;
+        }
+
+        public string Company {
+            get;
+            set;
+        }
+
+        public string Station {
+            get;
+            set;
+        }
+    }
+
+    public class Geometory {
+        public string Type {
+            get;
+            set;
+        }
+
+        public IList<JArray> Coordinates{
+            get;
+            set;
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("アプリ実行中");
+
+            // jsonデータを読み込んでデシリアライズしている
+            StreamReader sr = new StreamReader("stationdata.geojson");
+            var json_data = sr.ReadToEnd();
+            // デシリアライズで用いるLailDataプロパティ他は上の方に書いてあります
+            var deseriarize = JsonConvert.DeserializeObject<LailData>(json_data);
+
+            //var count = 1;
+
+            // デシリアライズしたデータの中から路線名と駅名と緯度を抜き出して表示
+            /*foreach (var json_parse_data in deseriarize.Features) {
+                var CoordinatesData = json_parse_data.Geometry.Coordinates;
+
+                foreach (var coord_data in CoordinatesData) {
+                    Console.WriteLine("{0}回目",count);
+                    Console.WriteLine("{0},{1}駅",json_parse_data.Properties.Linename,json_parse_data.Properties.Station);
+                    var coordinatesDataTest = coord_data[1];
+
+                    Console.WriteLine((double)coordinatesDataTest);
+                    count += 1;
+                }   
+            }*/
+
+            // デシリアライズしたデータをCSVに書き換え
+            using (var stream_writer = new System.IO.StreamWriter(@"station.csv")) {
+                foreach (var json_parse_data in deseriarize.Features) {
+                    var coordinates_data = json_parse_data.Geometry.Coordinates;
+                    var company_name = json_parse_data.Properties.Company;
+                    var line_name = json_parse_data.Properties.Linename;
+                    var station_name = json_parse_data.Properties.Station;
+
+                    foreach (var coord_data in coordinates_data) {
+                        var longitude = (double)coord_data[0];
+                        var latitude = (double)coord_data[1];
+                        Console.WriteLine("{0},{1},{2},{3},{4}",company_name,line_name,station_name,latitude,longitude);
+                        stream_writer.WriteLine("{0},{1},{2},{3},{4}",company_name,line_name,station_name,latitude,longitude);
+                    }
+                }
+            }
+        }
+    }
+}
